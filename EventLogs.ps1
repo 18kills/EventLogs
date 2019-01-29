4a3f3a75b833ca926362f7c9a2a511a9056175a8
Richard T Swierk <18kills.18kills@gmail.com>
2019-01-29 17:55:10 -0500

    Update EventLogs.ps1

--- EventLogs.ps1
+++ EventLogs.ps1
@@ -1,18 +1,34 @@
-$pathNotGiven='$home\Documents'
+Clear-host
+$pathNotGiven="$home\Documents"
 $user_input=read-host -Prompt "Please enter the process name to search for"
 $the_cmd=ps | where {$_.Processame -ilike "*$user_input*"}
-$to_save=Read-host "Do you want to see the results [Y|N]"
-if($to_save -eq 'Y')
+clear-host
+write-host 'Enter the number for the option you choose'
+write-host 'Enter nothing to print results to screen'
+write-host '1. Do you want to save the results'
+write-host '2. Do you want to send results via email'
+$option=Read-host "Do you want to save the results [Y|N]"
+if($option -eq 1)
 {
+    Clear-host
     Write-host 'Enter the full path and a file name'
     Write-host 'If you do not enter a path and file name it will save to Documents'
     $path=Read-host 'Enter file path'
+    clear-host
+    write-host -backgroundcolor black -foregroundcolor green $path
     if(!$path)
     {
+        clear-host
         $the_cmd | Export-Csv -NoTypeInformation -Path $pathNoGive'EventLogs.cvs'
+        write-host 'Saved to '$pathNoGive'EventLogs.cvs'
     }else{
+        clear-host
         $the_cmd | Export-Csv -NoTypeInformation -Path $path'.cvs'
+        write-host 'Saved to '$path'.cvs'
     }
+}elseif()
+{
+    
 }else{
     $the_cmd
 }
